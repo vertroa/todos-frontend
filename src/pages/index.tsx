d8845a4bb03693045a3f785d97181c5346f91aa5
JD <vertroa@pm.me>
2023-03-09 06:51:41 -0800

    implemented add button with hardcoded data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,45 @@
 import Head from 'next/head'
 import { TodoType } from '@/types/types'
 import Todo from '../components/Todo'
+import { useEffect, useState } from 'react'
 
  
 export default function Home({ todos }: { todos: TodoType[] }) {
+  const [todoListState, setTodoState] = useState<TodoType[]>(todos)
+
+  async function handleAddButton(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: 'test', description: 'New Todo Description', completed: false })
+    };
+
+    const res = await fetch('http://localhost:8080/todos', requestOptions)
+    const data: TodoType = await res.json()
+
+    setTodoState([
+      ...todoListState, 
+      data
+    ])
+  }
+
   return (
     <>
       <header className='flex items-center justify-between bg-red-500 py-4 px-64 text-3xl font-bold drop-shadow-md text-white '>
         <h1 className="">
           To Dos
         </h1>
-        <button className='hover:cursor-pointer'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        <button onClick={handleAddButton} className='hover:cursor-pointer'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8 hover:hover:fill-emerald-500">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
         </button>
       </header>
-      
-      {todos.map((todo) => 
-          <Todo todo={todo}/>
+
+      {todoListState.map((todo) => 
+          <Todo key={todo.id} todo={todo}/>
       )}
     </>
   )
@@ -35,3 +56,6 @@ export async function getServerSideProps() {
     }
   }
 }
+
+
+
