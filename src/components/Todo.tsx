2d1750343e9307a82ea1eab16384e3d22348fbbd
JD <vertroa@pm.me>
2023-03-14 05:01:33 -0700

    implemented responsive design and refactored/cleaned up codebase

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,17 @@
 import { TodoType } from '@/types/types'
 import { useState } from 'react'
 
+  // Saves the todo to the backend API when the user clicks out of the input field
+async function saveTodo(todo: TodoType) {
+  const requestOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title: todo.title, description: todo.description, completed: todo.completed })
+  };
+  const saveURL = `${process.env.NEXT_PUBLIC_API}/todos/${todo.id}`
+
+  const res = await fetch(saveURL, requestOptions)
+}
 
 export default function Todo(props: any) {
   // The todo state is used to store the todo's data
@@ -17,8 +28,8 @@ export default function Todo(props: any) {
     }))
   }
 
-  // Saves the todo to the backend API when the user clicks out of the input field
-  async function saveTodo(e: React.FocusEvent<HTMLHeadingElement>) {
+  // Updates the title or description of the todo when the user clicks out of the input field
+  function updateTodoField(e: React.FocusEvent<HTMLHeadingElement>) {
     e.preventDefault()
 
     if (todo.title === '') {
@@ -26,81 +37,97 @@ export default function Todo(props: any) {
         ...prev,
         title: 'Untitled Todo'
       }))
-    }
-  
-    const requestOptions = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: todo.title, description: todo.description, completed: false })
-    };
+    } 
 
-    await fetch(`http://localhost:8080/todos/${todo.id}`, requestOptions)
-
+    saveTodo(todo)
     setIsEditing(false)
   }
 
-  function handleKeyPress
-  (e: React.KeyboardEvent<HTMLInputElement>) {
-    console.log('test')
-    if (e.key=== 'Enter') {
-      blur()
-    }
+  // Toggles the todo's completed status
+  async function changeTodoStatus(e: React.MouseEvent<HTMLButtonElement>) {
+    setTodo(prev => ({
+      ...prev,
+      completed: !prev.completed
+    }))
+
+    // Create updated version of the todo to avoid state not updating in time
+    const updatedTodo = {...todo, completed: !todo.completed}
+    await saveTodo(updatedTodo)
   }
 
   return (
     <>
-      <div className='grid grid-cols-auto grid-cols-3 grid-rows-2 mx-64 px-3 py-3 bg-slate-100 rounded-lg my-4 drop-shadow-md'>
-          <div className='col-span-2 text-lg font-bold pb-2'>
-            {isEditing ?
-              <input 
-                type="text" 
-                name='title' 
-                value={todo.title} 
-                onChange={handleEdit} 
-                onBlur={saveTodo} 
-                className='outline-none px-2 bg-slate-100 focus:outline-slate-300 focus:rounded-lg focus:outline' 
-              />
-            :
-            <h3 
-              onClick={e => setIsEditing(prev => (!prev))} 
-              className='px-2'>
-              {todo.title}
-            </h3>
+    <div className='grid grid-cols-12 justify-center'>
+      <div className='col-span-1 md:col-span-3 xl:col-span-4'></div>
+      <div className='col-span-10 md:col-span-6 xl:col-span-4 grid grid-cols-5 grid-rows-2 px-3 py-3 bg-slate-100 rounded-lg my-4 drop-shadow-md'>
+        <div 
+        className='flex row-span-2 my-auto justify-center'>
+        <button 
+          className={todo.completed ? 
+            'border-2 w-7 h-7 rounded-full bg-red-500'
+          :
+            'border-2 w-7 h-7 rounded-full hover:border-red-500'
           }
-          </div>
+          onClick={changeTodoStatus}
+        >
+        </button>
+        </div>
 
-          <div className='flex row-span-1 items-center justify-end space-x-2'>
-            <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              onClick={event => props.handelDel(event, todo.id)} 
-              fill="none" 
-              viewBox="0 0 24 24" 
-              strokeWidth={1.5} 
-              stroke="currentColor" 
-              className="w-6 h-6 hover:cursor-pointer hover:fill-rose-400">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-            </svg>
-          </div>
+        <div className='col-span-3 text-lg font-bold'>
+          {isEditing ?
+            <input 
+              type="text" 
+              name='title' 
+              value={todo.title} 
+              onChange={handleEdit} 
+              onBlur={updateTodoField} 
+              className='outline-none  bg-slate-100 focus:outline-slate-300 focus:rounded-lg focus:outline' 
+            />
+          :
+          <h3 
+            onClick={e => setIsEditing(prev => (!prev))} 
+            className={todo.completed ? 'line-through' : ''}
+          >
+            {todo.title}
+          </h3>
+        }
+        </div>
 
-          <div className='mt-1 col-span-2'>
-            {isEditing ?
-              <input
-                type="text"
-                name='description'
-                value={todo.description}
-                onChange={handleEdit}
-                onBlur={saveTodo}
-                className='text-sm outline-none px-2 bg-slate-100 focus:outline-slate-300 focus:rounded-lg focus:outline'
-              />
-              :
-              <p
-                onClick={e => setIsEditing(prev => (!prev))}
-                className='text-sm px-2'>
-                {todo.description}
-              </p>
-            }
-          </div>
+        <div className='flex row-span-1 items-center justify-end space-x-2'>
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            onClick={event => props.handelDel(event, todo.id)} 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            strokeWidth={1.5} 
+            stroke="currentColor" 
+            className="w-6 h-6 hover:cursor-pointer hover:fill-rose-400">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+          </svg>
+        </div>
+
+        <div className='mt-1 col-span-4'>
+          {isEditing ?
+            <input
+              type="text"
+              name='description'
+              value={todo.description}
+              onChange={handleEdit}
+              onBlur={updateTodoField}
+              className='text-sm outline-none py-1 bg-slate-100 focus:outline-slate-300 focus:rounded-lg focus:outline'
+            />
+            :
+            <p
+              onClick={e => setIsEditing(prev => (!prev))}
+              className={todo.completed ? 'text-sm py-1 line-through' : 'py-1 text-sm'}
+            >
+              {todo.description}
+            </p>
+          }
+        </div>
       </div>
+    </div>
+      <div className='col-span-1 md:col-span-3 xl:col-span-4'></div>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,17 @@ import { useEffect, useState } from 'react'
 export default function Home({ todos }: { todos: TodoType[] }) {
   const [todoListState, setTodoListState] = useState<TodoType[]>(todos)
 
-  async function handleAddButton(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault()
-
+  // Add button to create new todo
+  async function handleAddButton(e: React.MouseEvent<HTMLButtonElement> | React.TouchEvent<HTMLButtonElement>) {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: 'test', description: 'New Todo Description', completed: false })
+      body: JSON.stringify({ title: 'New Todo', description: 'New Todo Description', completed: false })
     };
 
-    const res = await fetch('http://localhost:8080/todos', requestOptions)
+    const addURL = `${process.env.NEXT_PUBLIC_API}/todos`
+    console.log(addURL)
+    const res = await fetch(addURL, requestOptions)
     const data: TodoType = await res.json()
 
     setTodoListState([
@@ -25,32 +26,45 @@ export default function Home({ todos }: { todos: TodoType[] }) {
     ])
   }
 
-  async function handleDeleteButton(e: React.MouseEvent<SVGSVGElement>, id: number) {
+  // Delete button to delete todo
+  async function handleDeleteButton(e: React.MouseEvent<HTMLButtonElement>, id: number) {
     const requestOptions = {
       method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {'Content-Type': 'application/json'},
     };
     
-    const res = await fetch(`http://localhost:8080/todos/${id}`, requestOptions)
-    const data: TodoType = await res.json()
+    const deleteURL = `${process.env.NEXT_PUBLIC_API}/todos/${id}`
+    console.log(deleteURL)
+    const res = await fetch(deleteURL, requestOptions)
 
     setTodoListState(todoListState.filter((todo) => todo.id !== id))
   }
 
   return (
   <>
-    <header className='flex items-center justify-between bg-red-500 py-4 px-64 text-3xl font-bold drop-shadow-md text-white '>
-      <h1 className="">
-        To Dos
-      </h1>
-      <button onClick={handleAddButton} className='hover:cursor-pointer'>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8 hover:hover:fill-emerald-500">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      </button>
+    <header className='grid grid-cols-12 bg-red-500 py-4 text-3xl font-bold drop-shadow-md text-white '>
+      <div className='col-span-3'></div>
+      <div className='col-span-6 flex items-center justify-between'>
+        <h1 className="">
+          To Dos
+        </h1>
+        <button 
+          onClick={handleAddButton} 
+          className='cursor-pointer'>
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            strokeWidth={2} 
+            stroke="currentColor" 
+            className="cursor-pointer w-8 h-8 hover:hover:fill-emerald-500">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </button>
+      </div>
+      <div className='col-span-4'></div>
     </header>
 
-    {/* {console.log(todoListState)} */}
     {todoListState.map((todo) => 
         <Todo key={todo.id} handelDel={handleDeleteButton} todo={todo}/>
     )}
@@ -58,9 +72,8 @@ export default function Home({ todos }: { todos: TodoType[] }) {
   )
 }
 
-
 export async function getServerSideProps() {
-  const res = await fetch('http://localhost:8080/todos/')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/todos/`)
   const todos = await res.json()
 
   return {
